0f4a81a52fb2d5c9a2a2afda87e437a428c269f5
Julio Alberto Aranis Contreras <julioalbertoaraniscontreras@MacBook-Pro-de-Julio.local>
2024-01-18 12:46:45 +0100

    prepapre to deploy

--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,15 +1,11 @@
-'use client'
-
 import { useState } from 'react'
 import { useSearchParams } from 'next/navigation'
 import DetailsPayment from '@/ui/views/details-payment'
-import { Loader } from '@/ui/components/loader'
 import { Option } from '@/ui/components/input-select'
 import { Payment } from '@/ddd/domains/payment'
 import { prepareParamsToObject } from '@/ddd/domains/event-prepare-url-to-object'
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false)
   const paramsUrl = useSearchParams().get('params')
   const params = prepareParamsToObject(paramsUrl as string)
   const payment = params.payment as Payment
@@ -17,14 +13,7 @@ export default function Home() {
 
   return (
     <main className="lg:w-2/5 sm:w-4/5 flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      {
-        isLoading &&
-          <Loader />
-      }
-      {
-        !isLoading &&
-          <DetailsPayment payment={payment} coinTypes={coinTypes} />
-      }      
+      <DetailsPayment payment={payment} coinTypes={coinTypes} />
     </main>
   )
 }
